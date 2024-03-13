8cc9e87a702b6e5ff1ddb41a1003c5722c788605
claudeomosa <claudeomosa@gmail.com>
2024-03-13 04:58:05 +0300

    Config: Add SecurityConfig and WebMvcConfig for corsMapping

---
+++ api/src/main/java/creswave/api/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package creswave.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // URL of the Angular dev server
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}

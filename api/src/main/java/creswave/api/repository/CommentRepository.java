fbcb0951655cffdaec789a554f23d23e2e60696e
claudeomosa <claudeomosa@gmail.com>
2024-03-14 13:21:50 +0300

    fix: annotate repository classes with `@Repository`

--- api/src/main/java/creswave/api/repository/CommentRepository.java
+++ api/src/main/java/creswave/api/repository/CommentRepository.java
@@ -2,9 +2,11 @@ package creswave.api.repository;
 
 import creswave.api.model.BlogComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<BlogComment, Long> {
     List<BlogComment> findByPostId(Long postId);
 }
--- api/src/main/java/creswave/api/repository/PostRepository.java
+++ api/src/main/java/creswave/api/repository/PostRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<BlogPost, Long> {
     Page<BlogPost> findByUserId(Long userId, Pageable pageable);
     @Query("SELECT p FROM BlogPost p WHERE p.title LIKE %?1%"

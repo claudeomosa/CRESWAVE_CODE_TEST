3a8cccf3b2f55ffb75f90ea74fb66c1a15a04606
claudeomosa <claudeomosa@gmail.com>
2024-03-14 21:43:03 +0300

    UI: Add basic style for posts, single post and create post pages

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent}
+    {path: 'register', component: RegisterComponent},
+    {path: '', component: PostsComponent},
+    {path: "posts/:id", component: PostComponent},
+    {path: "create-post", component: CreatePostComponent},
 ];

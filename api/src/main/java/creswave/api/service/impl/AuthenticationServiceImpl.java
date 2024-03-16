4afdb91bb27e1ca041a47bdbb1ae63887b786504
claudeomosa <claudeomosa@gmail.com>
2024-03-16 08:09:35 +0300

    codeRefactor: use service interfaces and service implementation classes for auth and jwt

---
+++ api/src/main/java/creswave/api/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,132 @@
+package creswave.api.service.impl;
+
+import creswave.api.model.AuthenticationResponse;
+import creswave.api.model.Role;
+import creswave.api.model.Token;
+import creswave.api.model.User;
+import creswave.api.repository.TokenRepository;
+import creswave.api.repository.UserRepository;
+import creswave.api.service.AuthenticationService;
+import creswave.api.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.util.List;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenRepository tokenRepository;
+
+    @Autowired
+    public AuthenticationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager, TokenRepository tokenRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+        this.tokenRepository = tokenRepository;
+    }
+
+    @Override
+    public AuthenticationResponse register(User request) {
+
+        // check if user already exist. if exist than authenticate the user
+        if(userRepository.findByUsername(request.getUsername()).isPresent()) {
+            return new AuthenticationResponse(null, "User already exist");
+        }
+
+        User user = new User();
+        if (request.getFullname() == null || request.getFullname().isEmpty()) {
+            return new AuthenticationResponse(null, "Fullname is required");
+        } else {
+            user.setFullname(request.getFullname());
+        }
+        if (request.getUsername() == null || request.getUsername().isEmpty()) {
+            String username = request.getFullname().replaceAll("\\s", "").toLowerCase();
+            if(userRepository.findByUsername(username).isPresent()) {
+                username = username + "1";
+            }
+            user.setUsername(username);
+        } else {
+            user.setUsername(request.getUsername());
+        }
+        if (request.getEmail() == null || request.getEmail().isEmpty()) {
+            return new AuthenticationResponse(null, "Email is required");
+        } else {
+            if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+                return new AuthenticationResponse(null, "Email already exist");
+            }
+            user.setEmail(request.getEmail());
+        }
+        if (request.getPassword() == null || request.getPassword().isEmpty()) {
+            return new AuthenticationResponse(null, "Password is required");
+        } else {
+            if (request.getPassword().length() < 8) {
+                return new AuthenticationResponse(null, "Password must be at least 8 characters");
+            }
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+        }
+        if (request.getRole() == null) {
+            user.setRole(Role.valueOf("USER"));
+        } else {
+            user.setRole(request.getRole());
+        }
+
+        user = userRepository.save(user);
+
+        String jwt = jwtService.generateToken(user);
+
+        saveUserToken(jwt, user);
+
+        return new AuthenticationResponse(jwt, "User registration was successful");
+
+    }
+
+    @Override
+    public AuthenticationResponse authenticate(User request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        User user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String jwt = jwtService.generateToken(user);
+
+        revokeAllTokenByUser(user);
+        saveUserToken(jwt, user);
+
+        return new AuthenticationResponse(jwt, "User login was successful");
+
+    }
+
+    @Override
+    public void logout(String tokenString) {
+        Token token = tokenRepository.findByToken(tokenString).orElseThrow(() -> new IllegalArgumentException("Invalid token"));
+        token.setLoggedOut(true);
+        tokenRepository.save(token);
+    }
+
+    private void revokeAllTokenByUser(User user) {
+        List<Token> validTokens = tokenRepository.findAllTokensByUser(Math.toIntExact(user.getId()));
+        if(validTokens.isEmpty()) {
+            return;
+        }
+        validTokens.forEach(t-> t.setLoggedOut(true));
+        tokenRepository.saveAll(validTokens);
+    }
+    private void saveUserToken(String jwt, User user) {
+        Token token = new Token();
+        token.setToken(jwt);
+        token.setLoggedOut(false);
+        token.setUser(user);
+        tokenRepository.save(token);
+    }
+}
---
+++ api/src/main/java/creswave/api/service/impl/JwtServiceImpl.java
@@ -0,0 +1,68 @@
+package creswave.api.service.impl;
+
+import creswave.api.model.User;
+import creswave.api.service.JwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+    private final String SECRET_KEY = "0249b093c5c91cb1b967a28b491f80054e35d804885b5befa4c3dacaaebf5a71";
+    @Override
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    @Override
+    public boolean isValidUser(String token, UserDetails user) {
+        final String username = extractUsername(token);
+        return (username.equals(user.getUsername()) && !isTokenExpired(token));
+    }
+
+    @Override
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    @Override
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    @Override
+    public String generateToken(User user) {
+
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(getSigninKey())
+                .compact();
+    }
+
+    private SecretKey getSigninKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
